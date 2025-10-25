236f5dcbfc22245ac0cecc2cbe45adfababef7fd
Rehan Hussain <musaifhussain9@gmail.com>
2025-10-25 21:14:34 +0530

    Update api_ai.py

--- api_ai.py
+++ api_ai.py
@@ -340,3 +340,9 @@ if __name__ == '__main__':
     """)
     
     app.run(debug=True, host='0.0.0.0', port=5000)
+
+# Export for Vercel serverless
+# This allows Vercel to use the Flask app as a serverless function
+if __name__ != '__main__':
+    # Vercel will import this module, so we expose 'app'
+    application = app
